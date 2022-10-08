d610c36d7b476e9390f8efc8828ee259a53fcfe0
Sergio Duque <60938788+sergioduquem15@users.noreply.github.com>
2022-10-08 00:09:12 -0500

    Montecarlo, Apartado gráfico, guardar datos

--- p5_prueba/sketch.js
+++ p5_prueba/sketch.js
@@ -18,7 +18,12 @@ let magn;
 let magtx;
 let apagar;
 let encender;
-var numParticulas = 10;
+
+let tabla;
+let añadir;
+
+var numParticulas = 30;
+var magCampo = 50;  
 
 
 
@@ -48,19 +53,46 @@ function setup() {
 
   // Se sibuja el primer estado
   estado.dibujar();
+
+  // Grafico
+  plot = new GPlot(this, 600, 0,580,500);
+  plot.setTitleText("Promedio de la magnetizacion vs temperatura");
+  plot.getXAxis().setAxisLabelText("Temperatura [K]");
+  plot.getYAxis().setAxisLabelText("Magnetizacion promedio [A/m]");
+  plot.getYAxis().setNTicks(2);
+  plot.setXLim(0, 20);
+  plot.setYLim(0, 2.0);
+  plot.setGridLineWidth(2);
+  plot.setGridLineColor(210);
+  plot.setLineColor(120);
+  plot.setLineWidth(1);
   
   // Ingresar la cantidad de partículas
-  input = createInput( );
-  input.position(850, 560);
+  input = createInput('30');
+  input.size(50);
+  input.position(HEIGHT+70, 483);
   button = createButton("Enviar");
-  button.position(850 + 140, 560);
+  button.position(HEIGHT+127, 483);
+  
+  input2 = createInput(nfc(magCampo));
+  input2.size(50);
+  input2.position(HEIGHT+70, 520);
+  button = createButton("Enviar");
+  button.position(HEIGHT+127, 520);
 
   // Si se presiona se establece el numero de 
   // partículas ingresadas en el input y refresca
   button.mousePressed(() => {
     numParticulas = input.value();
+    magCampo = input2.value();
     setup();
   });
+  
+  tabla = new p5.Table();
+
+  tabla.addColumn("Temperatura");
+  tabla.addColumn("Magnetizacion_promedio");
+  
 
 }
 
@@ -73,40 +105,44 @@ function draw() {
 
   background(220);
   textSize(20);
+  
 
   /**
    * Se evoluciona el primer estado,
    * y cada que entra al loop, el
    * estado evoluciona 
    */
-  estado.evolucionar(temperatura);
+  estado.evolucionar(temperatura,magCampo);
   avmagn = estado.actualizar();
   magtx = estado.magtext()
   estado.dibujar();
 
 
   fill(0);
-  text("Temperatura: " + nfc(temperatura, 2), HEIGHT + 10, 520);
-  text("Magnetizacion: " + nfc(magtx), HEIGHT + 10, 550);
-
+  graph(temperatura,avmagn);
+	
+  
+  
   /**
    * Botones para continuar, pausar y reiniciar
    */
+  info();
+  textSize(20);
+  
+  
 
   temperatura += 0.1;
-  encender = createButton("Continuar");
-  encender.position(850, 520);
-  encender.mousePressed(loop);
-
-  apagar = createButton("Pausar");
-  apagar.position(950, 520);
-  apagar.mousePressed(noLoop);
-
-  reiniciar = createButton("Reiniciar");
-  reiniciar.position(1050, 520);
-  reiniciar.mousePressed(setup);
-
-  text("Número de partículas", HEIGHT + 10, 580);
+  botonesConf();
+  
+  text("L = ", HEIGHT + 30, 500);
+  text("H = ", HEIGHT + 27, 537);
+  
+  if (temperatura > 20){ noLoop()};
+  
+  añadir = tabla.addRow();
+  añadir.setNum("Temperatura", temperatura);
+  añadir.setNum("Magnetizacion_promedio", avmagn);
+  
 
 }
 
@@ -145,10 +181,10 @@ class estadoDelSistema {
   // ---- Funciones de la clase ----
   // -------------------------------
 
-  evolucionar = function (T) {
+  evolucionar = function (T,B) {
 
     // Guarda el resultado de la función Montecarlo
-    this.matrizDeEstado = MonteCarlo(this.matrizDeEstado, 1 / T);
+    this.matrizDeEstado = MonteCarlo(this.matrizDeEstado,1/T,B);
   }
 
 
@@ -164,11 +200,10 @@ class estadoDelSistema {
       }
     }
     this.magtxt = this.magnetizacion
-    if (this.magnetizacion < 0) { this.magabs += this.magnetizacion * (-1) }
-    else { this.magabs += this.magnetizacion }
+    if (this.magnetizacion < 0) { this.magabs = this.magnetizacion * (-1) }
+    else { this.magabs = this.magnetizacion }
 
-    this.promedioMagnetizacion += this.magabs / (numParticulas * numParticulas);
-
+    this.promedioMagnetizacion += this.magabs / (numParticulas * numParticulas) 
 
     return this.promedioMagnetizacion
   }
@@ -210,16 +245,13 @@ class estadoDelSistema {
     }
     return arreglo;
   }
-
-
-
 }
 
 
 // ----------------------------------
 // --------- MONTE CARLO ------------
 // ----------------------------------
-function MonteCarlo(configuracion, beta) {
+function MonteCarlo(configuracion, beta, campo) {
 
   var L = configuracion.length;
   nuevaConfig = [...configuracion]; // Copia de la configuracion
@@ -227,27 +259,27 @@ function MonteCarlo(configuracion, beta) {
   for (let i = 0; i < L; i++) {
     for (let j = 0; j < L; j++) {
 
-      var a = Math.floor(Math.random() * (L)); // 0,1,2,3,4
-      var b = Math.floor(Math.random() * (L));
+      var conf_i = configuracion[i][j];
 
-      var sigma = configuracion[a][b];
+      var config1 = configuracion[mod((i + 1), L)][j];
+      var config2 = configuracion[i][mod((j + 1), L)];
+      var config3 = configuracion[mod((i - 1), L)][j];
+      var config4 = configuracion[i][mod((j - 1), L)];
+      var conf_j = config1 + config2 + config3 + config4;
 
-      var config1 = configuracion[mod((a + 1), L)][b];
-      var config2 = configuracion[a][mod((a + 1), L)];
-      var config3 = configuracion[mod((a - 1), L)][b];
-      var config4 = configuracion[a][mod((b - 1), L)];
+      var E_i = -conf_i*conf_j - conf_i*campo; /// campo
+      var conf_ip = -1*conf_i;
+      var E_f = -conf_ip*conf_j - conf_i*campo; /// campo
 
-      var neighbors = config1 + config2 + config3 + config4;
+      var del_E = E_f - E_i;
 
-      var del_E = 2 * sigma * neighbors;
-
-      if (del_E < 0) {
-        sigma *= -1;
-      } else if (Math.random() < Math.exp(-1 * del_E * beta)) {
-        sigma *= -1;
+      if (del_E <= 0) {
+        conf_i = conf_ip;
+      } else if (Math.random() < Math.exp(-del_E * beta)) {
+        conf_i = conf_ip;
       }
 
-      nuevaConfig[a][b] = sigma;
+      nuevaConfig[i][j] = conf_i;
     }
   }
 
@@ -292,7 +324,68 @@ function centroMasa(n) {
 function mod(n, m) {
   return ((n % m) + m) % m;
 }
+function reset() {
+  numParticulas = 30;
+  magCampo = 50;
+  setup();
+}
 
+function info(){
+	textSize(15);
+    text("Caja de tamaño L = "+nfc(numParticulas)+ " que contiene N = "+nfc(numParticulas**2)+ " particulas.\nContenido en un Campo magnetico externo H = "+nfc(magCampo), 45,25);
+  }
+
+function guardar() {
+  //se guardan los elementos de la tabla en un archivo scv
+  noLoop();
+  saveTable(tabla, "datos.csv");
+};
+
+function graph(temp,mag){
+  plot.addPoint(temperatura,avmagn);
+  plot.beginDraw();
+  
+  plot.drawBox();
+  plot.drawXAxis();
+  plot.drawYAxis();
+  plot.drawTitle();
+  plot.drawGridLines(GPlot.BOTH);
+  plot.drawLines();
+  plot.drawPoints();
+  plot.endDraw();
+  
+  fill(150);
+  rect(HEIGHT + 330, 60, 195, 60);
+  fill(255);
+  text("Temperatura: " + nfc(temperatura, 2), HEIGHT + 340, 80);
+  text("Magnetizacion: " + nfc(magtx), HEIGHT + 335, 110);
+  fill(0);
+};
+
+function botonesConf(){
+  	fill(150);
+ 	rect(820, 500, 305, 55);
+   	//square(840, 500, 100);
+	fill(255);
+    encender = createButton("Continuar");
+    encender.position(830, 530);
+    encender.mousePressed(loop);
+
+    apagar = createButton("Pausar");
+    apagar.position(910, 530);
+    apagar.mousePressed(noLoop);
+
+    reiniciar = createButton("Reiniciar");
+    reiniciar.position(975, 530);
+    reiniciar.mousePressed(reset);
+
+    Guardar = createButton("Guardar");
+    Guardar.position(1050, 530);
+    Guardar.mousePressed(guardar);
+
+    text("Botones de configuracion:", HEIGHT + 250, 520);
+  	fill(0);
+};
 // -----------------------------------------
 // ------------ DIBUJA PARTÍCULAS ----------
 // -----------------------------------------
@@ -304,9 +397,10 @@ function cuadrado(x, y, estado, dimensiones) {
   }
   // Si no, pinta la partícula naranja
   else {
-    fill(255, 200, 100);
+    fill(200,0,0);
   }
 
   // Cuadrados en posicion (x,y) y el 80% de la dimensión
+  noStroke()
   square(x, y, dimensiones * 0.8);
 }
